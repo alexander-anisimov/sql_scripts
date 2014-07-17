a7122efe2a410f3146164a37cf21008316d9273e
alexander.anisimov <alexander.anisimov@admiralmarkets.com>
2014-07-17 12:35:04 +0300

    Modify script for Shrink database - added query for check process

--- Shrink database.sql
+++ Shrink database.sql
@@ -2,6 +2,10 @@
 DBCC SHRINKFILE (tempdev,5000)
 DBCC SHRINKDATABASE (tempdev,10)
 
+-- DB Status on Shrinkdatabase process
+USE [YourDB]
+SELECT percent_complete, start_time, status, command, estimated_completion_time, cpu_time, total_elapsed_time FROM sys.dm_exec_requests WHERE command = 'DbccFilesCompact'
+
 -- View open transactions
 DBCC opentran
 
